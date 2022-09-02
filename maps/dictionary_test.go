2fbcde4156a0c07e502c15d5155e87e5eb28db26
Mukesh Kumar Jangir <mukesh.jangir@gridinfocom.com>
2022-09-02 16:56:44 +0530

    practiced maps

---
+++ maps/dictionary_test.go
@@ -0,0 +1,57 @@
+package maps
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrNotFound
+
+		assertError(t, err, want)
+	})
+
+}
+
+func assertStrings(t testing.TB, got string, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	dictionary.Add("test", "this is a test")
+	want := "this is a test"
+
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
